87107b7f14d69bddf2829f86397984e415c1893d
Ionestar <kmahadew21@gmail.com>
2021-08-23 09:33:38 +0200

    update main()

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -10,23 +10,24 @@ use crate::test::{gen::generate, threaded::t_sell};
 use self::{read::readtovec, rund::simulate, threaded::t_buy};
 
 pub fn run(determinant: &str, pair: &str, startdate: u64) {
-    // let start = SystemTime::now();
-    // let since_the_epoch = start
-    //     .duration_since(UNIX_EPOCH)
-    //     .expect("Time went backwards");
-    // let in_ms =
-    //     since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
-    // gen::generate(determinant, pair, startdate, in_ms);
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    let in_ms =
+        since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
 
-    let (det, curr) = readtovec();
+    gen::generate(determinant, pair, startdate, in_ms);
+
+    // let (det, curr) = readtovec();
     // generate(determinant, pair, startdate, in_ms);
-    // // simulaute with given values;
-    println!("{}", simulate(-1.15, 168, 0.16, 151, det, curr));
+    // simulaute with given values;
+    // println!("{}", simulate(-1.15, 168, 0.16, 151, det, curr));
 
     // simulate buying optimisation in range r1..r2 with 8 threads
     // lower percent is = to r1/100;
     // upper percent is = to r2/100;
     // r1 == 10x percentage that you want
-    //t_buy(0, 1000, -1.15, 288, 168);
+    t_buy(0, 1000, -1.15, 288, 168);
     // t_sell(-1000, 0, 0.16, 151, 288);
 }
